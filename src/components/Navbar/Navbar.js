9baa68da68ebc01c61940b24ae72296ea1b8b258
AJ <amanda.elisabet.jordan@gmail.com>
2019-01-10 21:38:17 -0500

    ui updates

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,11 +6,13 @@ const Navbar = props => {
     <nav class='navbar sticky-top nav-fill bg-light justify-content-center'>
       <ul className='nav '>
         <li className='nav-item'>
-          <span className='navbar-brand mb-0 h1'>Click to start!</span>
-        </li>
-        <li className='nav-item'>
           <span className='navbar-brand mb-0 h1'>Score: {props.score}</span>
         </li>
+        <br />
+        <li className='nav-item'>
+          <span className='navbar-brand mb-0 h1'>Click to start!</span>
+        </li>
+        <br />
         <li className='nav-item'>
           <span className='navbar-brand mb-0 h1'>
             Wins: {props.wins} || Losses: {props.losses}
